fce25e3910af18c87d622083c8f54f2754cb02fa
Luis Benitez <luisbenitez02@gmail.com>
2017-07-13 23:21:16 -0500

    Un menu de contexto!
    Ahora cuando mantengas presionado el texto "Luis benitez" se abrira un menu que activa unos Toast.
    
    Para ello agregamos unos metodos al archivo .java y editamos algo el xml para hacer nuestro texto clickeable!

--- app/src/main/java/com/example/luisb/menuopscontextpopup/MainActivity.java
+++ app/src/main/java/com/example/luisb/menuopscontextpopup/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.luisb.menuopscontextpopup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvNombre = (TextView) findViewById(R.id.tvNombre);
+
+        registerForContextMenu(tvNombre);//nuestro textview esta disponible para levantar menu de contexto
     }
 
     //vamos a crear un menu de opciones
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_opciones, menu);//le pasamos nuestro menu (todo el layout)
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);//le pasamos nuestro menu ( el layout)
         return true;
     }
 
@@ -41,5 +52,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*Vamos a crear un menu contextual*/
+    /*Cuando mantengas presionado el textview se creara*/
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.menu_contexto,menu);
+    }
+
+    /*vamos a capturar el click sobre los elementos de nuestro menu contextual*/
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mEdit:
+                //acciones aqui
+                Toast.makeText(this,getResources().getString(R.string.text_click_edit),Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mDelete:
+                //acciones aqui
+                Toast.makeText(this,getResources().getString(R.string.text_click_delete),Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
+
     }
 }
