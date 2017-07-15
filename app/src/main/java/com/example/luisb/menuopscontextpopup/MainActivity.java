17529fdb30b0e24f8938e90642a3c9e37c3bc730
Luis Benitez <luisbenitez02@gmail.com>
2017-07-14 20:42:17 -0500

    Un menu popup
    Este menu saldra al tocar una imagen

--- app/src/main/java/com/example/luisb/menuopscontextpopup/MainActivity.java
+++ app/src/main/java/com/example/luisb/menuopscontextpopup/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.luisb.menuopscontextpopup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -81,5 +83,32 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onContextItemSelected(item);
 
+    }
+
+    /*Nuetro menu Popup (se abre simplemente al tocar un view)*/
+    public void levantarMenuPopup(View v){
+        ImageView imagen = (ImageView) findViewById(R.id.imgImagen);
+        PopupMenu popupMenu = new PopupMenu(this, imagen);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_popup,popupMenu.getMenu());
+
+        /*vamos a capturar cuando se seleccione una opcion*/
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.mView:
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.menu_view), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.mViewDetail:
+                        Toast.makeText(getBaseContext(), getResources().getString(R.string.menu_view_detail), Toast.LENGTH_LONG).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        popupMenu.show();//lo mostramos
     }
 }
